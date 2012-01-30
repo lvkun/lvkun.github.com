b389a96bf08ea66eba0436bf8bf4866de994596c
Bruce Lv <bruce.lv@nokia.com>
2012-01-30 22:44:39 +0800

    enable show md document.

--- js/staticblog.js
+++ js/staticblog.js
@@ -1,32 +1,45 @@
 var blog = {
     posts : null,
-    loadPost : function(path) {
-
+    con: new Showdown.converter(),
+    loadPost : function() {
+        // load post according to location.hash
+        var hash = location.hash;
+        var path = "";
+        if(hash.length == 0) {
+            // load the newest post
+            path = blog.posts[0].path;
+        } else {
+            // load the post defined in hash(date+title)
+            path = hash.replace(/^#/, '' );
+        }
+        
+        $.ajax({
+            url : "_post/" + path + ".md",
+            dataType : 'text',
+            success : blog.postLoaded
+        });
     },
     postLoaded : function(data) {
-
+        var post_content = blog.con.makeHtml(data);
+        $("<div id='top_panel'>").appendTo($("#wrapper"));
+        $("<div class='post'>").appendTo($("#wrapper")).html(post_content);
+        $("<div id='bottom_panel'>").appendTo($("#wrapper"));
     },
     indexLoaded : function(data) {
-        posts = data;
+        blog.posts = data;
 
-        if(posts.length == 0) {
+        if(blog.posts.length == 0) {
             console.log("no post exist");
             return;
         }
 
-        var hash = location.hash;
-        if(hash.length == 0) {
-            // load the newest post
-            console.log(posts[0].path);
-        } else {
-            // load the post defined in hash(date+title)
-
-        }
+        blog.loadPost();
 
         $(window).hashchange(function() {
-            var hash = location.hash;
-
-            // load the post defined in hash(date+title)
+            // clear exist content
+            $("#wrapper").html("");
+            
+            blog.loadPost();
         });
     }
 }
