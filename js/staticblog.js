a62db18eae1a21b674052f416e6681b158f7bcfa
Bruce Lv <lvkun2006@gmail.com>
2012-02-03 22:36:35 +0800

    basic index layout

--- js/staticblog.js
+++ js/staticblog.js
@@ -2,17 +2,38 @@ var blog = {
     posts : null,
     current_path: "",
     con: new Showdown.converter(),
-    loadPost : function() {
-        // load post according to location.hash
+    updateIndex: function() {
+        $(blog.posts).each(function(){
+            var $item_value_list = $("<ul class='index-item-row'>").appendTo($("#index-list"));
+            
+            var $post_title = $("<li class='post-title'>").appendTo($item_value_list);
+            $("<a>").appendTo($post_title).text(this.title).attr("href", "#"+this.path);
+            
+            $("<li class='post-date'>").appendTo($item_value_list).text(this.date);
+            var $post_tags = $("<li class='post-tags'>").appendTo($item_value_list);
+            $(this.tags).each(function(){
+                $("<a class='post-tag-href'>").appendTo($post_tags).text(this.toString()+";").attr("href", "##"+this.toString());
+            });
+            //$("<ul class='index-seperator'>").appendTo($("#index-list"));
+        });
+    },
+    showIndex: function() {
+        $("#wrapper").hide();
+        $("#index").show();
+    },
+    updateContent : function() {
+        // load post or show index according to location.hash
         var hash = location.hash;
+        blog.current_path = hash.replace(/^#/, '' );
         
-        if(hash.length == 0) {
-            // load the newest post
-            blog.current_path = blog.posts[0].path;
-        } else {
-            // load the post defined in hash(date+title)
-            blog.current_path = hash.replace(/^#/, '' );
+        if(blog.current_path.length == 0) {
+            blog.showIndex();
+            return;
         }
+        
+        $("#index").hide();
+        $("#wrapper").show();
+        
         // clear exist content
         $("#post").html("");
         
@@ -25,10 +46,8 @@ var blog = {
     updateHref : function(aClass, path){
         if(path.length == 0) {
             $("." + aClass).removeAttr("href");
-            $("." + aClass).addClass("noref");
         } else {
             $("." + aClass).attr("href", "#" + path);
-            $("." + aClass).removeClass("noref");
         }
     },
     postLoaded : function(data) {
@@ -39,34 +58,35 @@ var blog = {
             if(blog.posts[i].path == blog.current_path){
                 if(i>0){
                     // not first post
-                    blog.updateHref("prev_a", blog.posts[i-1].path);
+                    blog.updateHref("prev-a", blog.posts[i-1].path);
                 }
                 else{
-                    blog.updateHref("prev_a", "");
+                    blog.updateHref("prev-a", "");
                 }
                 
                 if(i<blog.posts.length-1){
                     // not last post
-                    blog.updateHref("next_a", blog.posts[i+1].path);
+                    blog.updateHref("next-a", blog.posts[i+1].path);
                 }
                 else{
-                    blog.updateHref("next_a", "");
+                    blog.updateHref("next-a", "");
                 }
             }
         }
     },
     indexLoaded : function(data) {
         blog.posts = data;
-
+        blog.updateIndex();
+        
         if(blog.posts.length == 0) {
             console.log("no post exist");
             return;
         }
 
-        blog.loadPost();
+        blog.updateContent();
 
         $(window).hashchange(function() {
-            blog.loadPost();
+            blog.updateContent();
         });
     }
 }
