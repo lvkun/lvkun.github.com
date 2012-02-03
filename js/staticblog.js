db1a3936ce80f9b77382479c862f498a93a3b2a9
Bruce Lv <lvkun2006@gmail.com>
2012-02-04 00:52:59 +0800

    support tag filter

--- js/staticblog.js
+++ js/staticblog.js
@@ -6,33 +6,64 @@ function removeDuplicate(arr){
             map[this] = this;
             ret_arr.push(this.toString());
         }
-        
     });
 
     return ret_arr;
 }
 
 var blog = {
+    /* member variable */
     posts: null,
     post_tags: [],
+    filter_tags: [],
     current_path: "",
     con: new Showdown.converter(),
+    
+    /* functions */
+    filterPost: function(tags){
+        var should_show = false;
+        if(blog.filter_tags.length > 0){
+            for(var i in blog.filter_tags){
+                for(var j in tags){
+                    if(blog.filter_tags[i] == tags[j]){
+                        should_show = true;
+                    }
+                }
+            }
+        }
+        else{
+            should_show = true;
+        }
+        return should_show;
+    },
+    updateIndexItem: function(post_index_data){
+        var $item_value_list = $("<ul class='index-item-row'>").appendTo($("#index-list"));
+            
+        var $post_title = $("<li class='post-title'>").appendTo($item_value_list);
+        $("<a>").appendTo($post_title).text(post_index_data.title).attr("href", "#"+post_index_data.path);
+        
+        $("<li class='post-date'>").appendTo($item_value_list).text(post_index_data.date);
+        var $post_tags = $("<li class='post-tags'>").appendTo($item_value_list);
+        $(post_index_data.tags).each(function(){
+            $("<a class='post-tag-href'>").appendTo($post_tags).text(this.toString()+";").attr("href", "#@"+this.toString());
+        });
+        blog.post_tags = blog.post_tags.concat(post_index_data.tags);
+    },
     updateIndex: function() {
+        // clear index content
+        $("#index-list").html("");
+        
         $(blog.posts).each(function(){
-            var $item_value_list = $("<ul class='index-item-row'>").appendTo($("#index-list"));
+            if(!blog.filterPost(this.tags)){
+                // should not be shown
+                // because tags not in filter tags
+                return;
+            }
             
-            var $post_title = $("<li class='post-title'>").appendTo($item_value_list);
-            $("<a>").appendTo($post_title).text(this.title).attr("href", "#"+this.path);
-            
-            $("<li class='post-date'>").appendTo($item_value_list).text(this.date);
-            var $post_tags = $("<li class='post-tags'>").appendTo($item_value_list);
-            $(this.tags).each(function(){
-                $("<a class='post-tag-href'>").appendTo($post_tags).text(this.toString()+";").attr("href", "##"+this.toString());
-            });
-            blog.post_tags = blog.post_tags.concat(this.tags);
+            blog.updateIndexItem(this);
         });
         
-        blog.post_tags = removeDuplicate(blog.post_tags)
+        blog.post_tags = removeDuplicate(blog.post_tags);
     },
     showIndex: function() {
         $("#wrapper").hide();
@@ -43,7 +74,16 @@ var blog = {
         var hash = location.hash;
         blog.current_path = hash.replace(/^#/, '' );
         
-        if(blog.current_path.length == 0) {
+        var tags = blog.current_path.split("@");
+        
+        blog.filter_tags = [];
+        if(tags.length > 1){
+            blog.filter_tags = blog.filter_tags.concat(tags.slice(1));
+        }
+        
+        blog.updateIndex();
+        
+        if(blog.current_path.length == 0 || blog.filter_tags.length > 0) {
             blog.showIndex();
             return;
         }
@@ -67,6 +107,8 @@ var blog = {
             $("." + aClass).attr("href", "#" + path);
         }
     },
+    
+    /* event handler */
     postLoaded : function(data) {
         var post_content = blog.con.makeHtml(data);
         $("#post").html(post_content);
@@ -93,7 +135,6 @@ var blog = {
     },
     indexLoaded : function(data) {
         blog.posts = data;
-        blog.updateIndex();
         
         if(blog.posts.length == 0) {
             console.log("no post exist");
